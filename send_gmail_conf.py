ae4f293bf612c246ec4f471d5bf8af8d831d1b0d
Aubrey Moore <aubreymoore@guam.net>
2019-07-25 22:29:34 +1000

    minors

---
+++ send_gmail_conf.py
@@ -0,0 +1,6 @@
+YOUR_GMAIL_ADDRESS = 'aubreymoore2013@gmail.com'
+YOUR_GMAIL_PASSWORD = 'Canada_12'
+RECIPIENT_LIST = 'aubreymoore@guam.net'
+SUBJECT = 'Test'
+TEXT = 'Hi there'
+ATTACHMENT_LIST = ['1Mcolors.png']
